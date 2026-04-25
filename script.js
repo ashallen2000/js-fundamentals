117216edea6f96f4146e4f3d2f665c8416e4e11b
ashallen2000 <85514779+ashallen2000@users.noreply.github.com>
2026-04-25 15:36:10 +0100

    trying to build basic dom understanding

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const title = "Javascript Fundamentals";
 let taskCounter = 0;
 let darkMode = true;
+
+
+const addTask = (taskName) => {
+    console.log("Adding task: " + taskName);
+};
+
+const toggleDarkMode = () => {
+    console.log("dark mode toggled");
+};
+
+
+addTask("buy milk");
+toggleDarkMode();
+
+
+const counterHeading = document.querySelector("#task-count")
+
+counterHeading.textContent = ("tasks: " + taskCounter)
