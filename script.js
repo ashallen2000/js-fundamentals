2a13b0bd0ab431e267dec06d914b2c5cc45fa231
ashallen2000 <85514779+ashallen2000@users.noreply.github.com>
2026-04-26 12:40:00 +0100

    scrapped initial learning - want to make it relevant to something I enjoy. I'm creating a quiz that is going to teach me the basics of arrays, objects.

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const title = "Javascript Fundamentals";
-let taskCounter = 0;
-let darkMode = true;
+/*
+const questions = [
+    "A male Gerudo is born into the tribe every how many years?",
+    "What is the name of Link's fairy in Ocarina of Time?",
+    "What console was Twilight Princess originally released on?",
+    "What happens to the Mirror of Twilight at the end of Twilight Princess?",
+    "What is the first song that Link learns in Ocarina of time?"
+];
 
+console.log(questions[1])
 
-const addTask = (taskName) => {
-    console.log("Adding task: " + taskName);
-};
+basic array practise - moving to putting questions into an object now
+*/
 
-const toggleDarkMode = () => {
-    console.log("dark mode toggled");
-};
+/*
+const question1 = {
+    text: "A male Gerudo is born into the tribe every how many years?",
+    answer: "One is born into the tribe every 100 years."
+}
 
-
-addTask("buy milk");
-toggleDarkMode();
-
-
-const counterHeading = document.querySelector("#task-count")
-
-counterHeading.textContent = ("tasks: " + taskCounter)
+console.log(question1.text)
+console.log(question1.answer)
+practising using objects. now to combine both arrays and objects.
+/*
